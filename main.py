f50411a1cfefdf0dbcfdf648858ab2c12a5f0311
Simone Pio Caronia <simpio99@libero.it>
2024-01-16 17:02:45 +0100

    add arguments parser

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from services.fed_learning import start_fed_averaging_mnist_simulation
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+                    prog='2-way trusted FedAveraging',
+                    description='Thesis project by Caronia Simone Pio')
+    parser.add_argument('-he', '--homomorphic_encryption',
+                    action='store_true')
+    parser.add_argument('-zk', '--zk_proof',
+                    action='store_true')
+    parser.add_argument('-dv', '--disable_verbose',
+                    action='store_true')
+    return parser.parse_args()
 
 #TODO add argparse
-if __name__ == "__main__":
+if __name__ == "__main__":    
+    args = parse_args()
+
     start_fed_averaging_mnist_simulation(
         num_clients=10,
-        num_rounds=10,
+        num_rounds=5,
         fraction_fit=1.0,
         fraction_evaluate=0.3,
-        enable_he=True,
-        enable_zk_proof=False,
-        verbose=True
+        enable_he=args.homomorphic_encryption,
+        enable_zk_proof=args.zk_proof,
+        verbose=~args.disable_verbose
     )
