902c0346ffd49eb75a44ff6598d14fc963a18675
Simone Pio Caronia <simpio99@libero.it>
2024-01-17 08:55:56 +0100

    add autoscale false in tenseal

--- services/ckks_he.py
+++ services/ckks_he.py
@@ -6,6 +6,7 @@ from typing import Tuple, List
 def generate_context_and_secret(poly_modulus_degree: int=4096, plain_modulus: int=1032193, global_scale_exp: int = 40) -> Tuple[ts.Context, ts_enc_context.SecretKey]:
     ckks_context = ts.context(ts.SCHEME_TYPE.CKKS, poly_modulus_degree=poly_modulus_degree, plain_modulus=plain_modulus)
     ckks_context.global_scale = 2**global_scale_exp
+    ckks_context.auto_rescale = False #It seems to decrease the used memory
     ckks_secret_key = ckks_context.secret_key()
     ckks_context.make_context_public()
     return ckks_context, ckks_secret_key
